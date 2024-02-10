e09e77e823e821bd60c5d516d63e87178ceb7503
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-02-10 18:46:01 -0500

    "Completed test project"

--- DeliveryService.Tests/DeliveryService_Tests.cs
+++ DeliveryService.Tests/DeliveryService_Tests.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeliveryService.Data;
+using DeliveryService.Repository;
 
 namespace DeliveryService.Tests
 {
+    [TestClass]
     public class DeliveryService_Tests
     {
-        
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+            Delivery myDelivery = new Delivery();
+            myDelivery.OrderName = "Shoes";
+            myDelivery.ItemQuantity = 2;
+            myDelivery.ItemNumber = 3344;
+            myDelivery.DeliveryStatus = "canceled";
+            myDelivery.OrderDate = "03/11/2023";
+
+            Assert.AreEqual("Shoes", myDelivery.OrderName);
+            Assert.AreEqual(3, myDelivery.ItemQuantity);
+            Assert.AreEqual(3344, myDelivery.ItemQuantity);
+            Assert.AreEqual("Canceled", myDelivery.DeliveryStatus);
+            Assert.AreEqual("03/11/2023", myDelivery.OrderDate);
+
+        }
     }
 }
